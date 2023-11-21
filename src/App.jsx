aa5b64fcfdc4a1200e15fa610fc8d7d77dd3c539
Mohammed <sajidcurious9@gmail.com>
2023-11-21 22:20:06 +0530

    modifying App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/Themes.js";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
 
 function App() {
-  return <>Portfolio</>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <p>Portfolio</p>
+    </ThemeProvider>
+  );
 }
 
 export default App;
