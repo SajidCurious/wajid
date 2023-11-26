489d3da178f1a6bf11088421376c5bc5e004670c
Mohammed <sajidcurious9@gmail.com>
2023-11-26 20:33:49 +0530

    added required items to the HeroSection component

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,4 +1,23 @@
 import React from "react";
+import HeroBgAnimation from "../HeroBgAnimation";
+import {
+  HeroContainer,
+  HeroBg,
+  HeroLeftContainer,
+  Img,
+  HeroRightContainer,
+  HeroInnerContainer,
+  TextLoop,
+  Title,
+  Span,
+  SubTitle,
+  SocialMediaIcons,
+  SocialMediaIcon,
+  ResumeButton,
+} from "./HeroStyle";
+import HeroImg from "../../images/HeroImage.jpg";
+import Typewriter from "typewriter-effect";
+import { Bio } from "../../data/constants";
 
 const HeroSection = () => {
   return <div>HeroSection</div>;
