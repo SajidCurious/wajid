a0bd42b34a7d8f1de74599fb80c18021494a86ec
Mohammed <sajidcurious9@gmail.com>
2023-11-26 22:41:20 +0530

    building hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroBgAnimation from "../HeroBgAnimation";
+import HeroBgAnimation from "/src/components/HeroBgAnimation/HeroBgAnimation";
 import {
   HeroContainer,
   HeroBg,
@@ -11,16 +11,49 @@ import {
   Title,
   Span,
   SubTitle,
-  SocialMediaIcons,
-  SocialMediaIcon,
   ResumeButton,
-} from "./HeroStyle";
-import HeroImg from "../../images/HeroImage.jpg";
+} from "./Style";
+import HeroImg from "/HeroImage.jpg";
 import Typewriter from "typewriter-effect";
 import { Bio } from "../../data/constants";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <Title>
+              Hi, I am <br /> {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display">
+              Check Resume
+            </ResumeButton>
+          </HeroLeftContainer>
+
+          <HeroRightContainer id="Right">
+            <Img src={HeroImg} alt="hero-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
 };
 
 export default HeroSection;
