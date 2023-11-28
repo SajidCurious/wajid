454e1aef925be9cc4b6a3fec281b21cd1b1046ae
Mohammed <sajidcurious9@gmail.com>
2023-11-28 13:57:05 +0530

    building skills section

--- src/data/constants.jsx
+++ src/data/constants.jsx
@@ -1,11 +1,6 @@
 export const Bio = {
   name: "Mohammed Sajid",
-  roles: [
-    "Frontend Developer",
-    "Android Developer",
-    "UI/UX Designer",
-    "Programmer",
-  ],
+  roles: ["Frontend Developer", "Programmer"],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/SajidCurious",
