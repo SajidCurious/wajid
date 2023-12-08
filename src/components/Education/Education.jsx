123ea02eb59cb5d75dba08bce1d320054e5372b2
Mohammed <sajidcurious9@gmail.com>
2023-12-08 15:29:28 +0530

    imported required items

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { Container, Wrapper, Title, Desc, TimelineSection } from "./Style";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import { education } from "../../data/constants";
+import EducationCard from "../Cards/EducationCard";
 
 const Education = () => {
   return <div>Education</div>;
