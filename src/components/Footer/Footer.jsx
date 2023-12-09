e1cd24a52831a9b3a1b7e16a7dfeeb5e201d0592
Mohammed <sajidcurious9@gmail.com>
2023-12-09 21:42:30 +0530

    imported required items for the footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { Bio } from "../../data/constants";
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
