f970609686c141b41f52617f57d29dd88ab4d29b
Mohammed <sajidcurious9@gmail.com>
2023-12-09 16:50:11 +0530

    imported required items for the contact form component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { Snackbar } from "@mui/material";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  ContactForm,
+  ContactTitle,
+  ContactInput,
+  ContactInputMessage,
+  ContactButton,
+} from "./Style";
+
+const Contact = () => {
+  return <div>Contact</div>;
+};
+
+export default Contact;
