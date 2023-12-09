156ec139e98abe49aa489c655cb903eaa134457b
Mohammed <sajidcurious9@gmail.com>
2023-12-09 15:58:11 +0530

    completed it

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -10,7 +10,32 @@ import { education } from "../../data/constants";
 import EducationCard from "../Cards/EducationCard";
 
 const Education = () => {
-  return <div>Education</div>;
+  return (
+    <Container id="education">
+      <Wrapper>
+        <Title>Education</Title>
+        <Desc>
+          My education has been a journey of self-discovery and growth. My
+          educational details are as follows.
+        </Desc>
+        <TimelineSection>
+          <Timeline>
+            {education.map((education, index) => (
+              <TimelineItem key={education.id}>
+                <TimelineContent sx={{ py: "12px", px: 2 }}>
+                  <EducationCard education={education} />
+                </TimelineContent>
+                <TimelineSeparator>
+                  <TimelineDot variant="outlined" color="secondary" />
+                  <TimelineConnector style={{ background: "#854CE6" }} />
+                </TimelineSeparator>
+              </TimelineItem>
+            ))}
+          </Timeline>
+        </TimelineSection>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Education;
